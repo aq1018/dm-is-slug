697da59391a3f340fc999cac3b1d2014814d0380
Aaron Qian <aq1018@gmail.com>
2013-09-13 02:30:48 -0700

    trying to make travis CI work with mysql and postgres

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,23 @@ require 'dm-core/spec/lib/adapter_helpers'
 require 'dm-is-slug'
 require 'dm-migrations'
 
+# dm-core 1.0.2 does not allow configuration of spec db settings.
+# This monkey patch is ripped from dm-core master branch.
+# https://github.com/datamapper/dm-core/blob/master/lib/dm-core/spec/setup.rb#L134
+class DataMapper::Spec::Adapters::Adapter
+  def username
+    ENV.fetch('DM_DB_USER', 'datamapper')
+  end
+
+  def password
+    ENV.fetch('DM_DB_PASSWORD', 'datamapper')
+  end
+
+  def host
+    ENV.fetch('DM_DB_HOST', 'localhost')
+  end
+end
+
 DataMapper::Spec.setup!
 
 Spec::Runner.configure do |config|
